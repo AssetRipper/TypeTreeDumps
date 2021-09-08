2b52935ffcd10a14d2c3730e0de7892c7540d3a6
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-08 04:09:52 -0400

    remove files due to new format

---
+++ StructsDump/release/diff.sh
@@ -0,0 +1,7 @@
+vers=($(ls *.dump | sort -t. -k1,1n -k2,2n -k3,3n))
+rm -f diffs.diff
+for ((i=0; i<${#vers[@]}-1; i++)); 
+do
+    echo ${vers[i]} .. ${vers[i+1]}
+    diff -wBu0 --label=${vers[i]} --label=${vers[i+1]} ${vers[i]} ${vers[i+1]} >> diffs.diff
+done
