dda3df5484d539c89f89e8d738ef739b794b4d3f
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-08 14:51:03 -0400

    add more 2017 files

--- diff_all.sh
+++ diff_all.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo Running difference on class json files
 cd Classes
 ./diffs.sh
 cd ..
 
+echo
+echo Running difference on runtime type info dumps
 cd RTTI_Dump
 ./diffs.sh
 cd ..
