b90a9be2e9d926682985a27e90ee168394735c7a
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-08 20:02:06 -0400

    some 2018 files

--- configuration.sh
+++ configuration.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 path_to_7zip="/mnt/c/Program Files/7-Zip/7z.exe"
-path_to_UnitySetup="/mnt/g/TypeTreeDumps/UnityInstallers"
+path_to_UnitySetup="/mnt/e/UnityInstallers"
 path_to_UnityInstallations="/mnt/g/TypeTreeDumps/UnityInstallations"
 
 path_to_DumperExe="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/TypeTreeDumper.BootStrapper.exe"
 path_to_OutputFolder="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/Output"
-
-current_assembly_version="v000"
