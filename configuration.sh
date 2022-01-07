661329fafae9230f1e20a37036b9a4a71141e7bf
Jeremy Pritts <jrpritts7@gmail.com>
2022-01-07 15:09:51 -0500

    update scripts

--- configuration.sh
+++ configuration.sh
@@ -2,7 +2,7 @@
 
 path_to_7zip="/mnt/c/Program Files/7-Zip/7z.exe"
 path_to_UnitySetup="/mnt/e/UnityInstallers"
-path_to_UnityInstallations="/mnt/g/TypeTreeDumps/UnityInstallations"
+path_to_UnityInstallations="/mnt/f/TypeTreeDumps/UnityInstallations"
 
 path_to_DumperExe="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/TypeTreeDumper.BootStrapper.exe"
 path_to_OutputFolder="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/Output"
