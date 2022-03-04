b73ef471b95eb58f499f0e9117a11e033635600c
Jeremy Pritts <jrpritts7@gmail.com>
2022-03-04 15:22:06 -0500

    add some missing info json's

--- configuration.sh
+++ configuration.sh
@@ -4,5 +4,5 @@ path_to_7zip="/mnt/c/Program Files/7-Zip/7z.exe"
 path_to_UnitySetup="/mnt/e/UnityInstallers"
 path_to_UnityInstallations="/mnt/f/TypeTreeDumps/UnityInstallations"
 
-path_to_DumperExe="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/TypeTreeDumper.BootStrapper.exe"
-path_to_OutputFolder="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net472/Output"
+path_to_DumperExe="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net6.0-windows/TypeTreeDumper.BootStrapper.exe"
+path_to_OutputFolder="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Release/net6.0-windows/Output"
