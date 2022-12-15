f2ce2c30736ce2c2422aea58d32ed5b8bd38654b
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-15 18:48:48 -0500

    new versions

--- configuration.sh
+++ configuration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 path_to_7zip="/mnt/c/Program Files/7-Zip/7z.exe"
-path_to_UnitySetup="/mnt/e/UnityInstallers"
-path_to_UnityInstallations="/mnt/f/TypeTreeDumps/UnityInstallations"
+path_to_UnitySetup="/mnt/f/UnityInstallers"
+path_to_UnityInstallations="/mnt/g/TypeTreeDumps/UnityInstallations"
 
 path_to_DumperExe="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Debug/net6.0-windows/TypeTreeDumper.BootStrapper.exe"
 path_to_OutputFolder="/mnt/e/repos/TypeTreeDumper/TypeTreeDumper.Bootstrapper/bin/Debug/net6.0-windows/Output"
